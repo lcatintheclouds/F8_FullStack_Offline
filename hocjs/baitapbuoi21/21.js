7b4d2050a96528cbe57cf0fb48e535a27b95ffd8
lcatintheclouds <lcatintheclouds@gmail.com>
2024-06-07 11:44:26 +0700

    update bai tap

--- hocjs/baitapbuoi21/21.js
+++ hocjs/baitapbuoi21/21.js
@@ -1,56 +1,79 @@
 // Bài 1:
-var arr = [1, 0, 5, 8, 9, 6];
-function minMax(array) {
+var array = [48, 10, 2, 8, 9, 17];
+function minMax() {
   var min = array[0];
-  var minLocation = 0;
+  var indexMin = 0;
   var max = array[0];
-  var maxLocation = 0;
+  var indexMax = 0;
   for (var i = 1; i < array.length; ++i) {
     if (min > array[i]) {
       min = array[i];
-      minLocation = i;
+      indexMin = i;
     }
     if (max < array[i]) {
       max = array[i];
-      maxLocation = i;
+      indexMax = i;
     }
   }
 
   console.log("Bài 1:");
+  console.log("Cho mảng: ", array);
   console.log("Số nhỏ nhất là: ", min);
-  console.log("Vị trí của số nhỏ nhất là: ", minLocation);
+  console.log("Vị trí của số nhỏ nhất là: ", indexMin);
   console.log("Số lớn nhất là: ", max);
-  console.log("Vị trí của số lớn nhất là: ", maxLocation);
+  console.log("Vị trí của số lớn nhất là: ", indexMax);
 }
-minMax(arr);
+minMax(array);
 console.log("------------------------------------------------------------");
 
 // bài 2: -------------------------------------------------------------------------
 console.log("Bài 2:");
-var primeNumber = [2, 3, 5, 7, 11, 13];
-/* function hello() {
-  var check = true;
-  if (n % 1 !== 0 || n <= 1) {
-    check = false;
-    console.log(`Không có số nguyên tố`);
-  } else {
-    for (var i = 2; i < n; i++) {
-      if (n % i === 0) {
-        check = false;
-        break;
-        console.log(`Không có số nguyên tố`);
-      }
+// var primeNumber = [2, 3, 5, 7, 11, 13];
+// var primeNumber = [0, 1, 4, 6];
+var primeNumber = [2, 3, 8, 10, 4];
+console.log("Cho mảng:", primeNumber);
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+  for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
   }
-} */
-// hello(n);
-
-var getTotal = 0;
-function arithmeticMean() {
-  for (var i = 0; i < primeNumber.length; i++) {
-    getTotal = getTotal + primeNumber[i];
-  }
-  var average = getTotal / primeNumber.length;
-  console.log(average);
+  return true;
 }
-arithmeticMean(primeNumber);
+function averagePrimeNumber(arr) {
+  var sum = 0;
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (isPrime(arr[i])) {
+      sum += arr[i];
+      count++;
+    }
+  }
+  if (count !== 0) {
+    var averageNumber = sum / count;
+    console.log("Trung bình các số nguyên tố trong mảng: ", averageNumber);
+  } else {
+    console.log("Không có số nguyên tố trong mảng.");
+    return "Không có số nguyên tố trong mảng.";
+  }
+}
+averagePrimeNumber(primeNumber);
+console.log("------------------------------------------------------------");
+
+// Bài 3: -----------------------------------------------------------
+console.log("Bài 3:");
+var arr3 = [2, 3, 5, 2, 4, 3, 1, 4, 6];
+var newArr3 = [];
+for (var i in arr3) {
+  if (newArr3.indexOf(arr3[i]) === -1) {
+    newArr3[newArr3.length] = arr3[i];
+  }
+}
+console.log("Cho mảng:", arr3);
+console.log("Sau lọc trùng ta được:", newArr3);
+console.log("------------------------------------------------------------");
+
+// Bài 4: -----------------------------------------------------------
